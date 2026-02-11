5d146cdfd323c903dc9f89d154c0f425eac85f5c
Mark Hollrah <mhollrah0001@gmail.com>
2026-02-11 18:49:46 -0500

    Javascript Added

---
+++ JavaScript/script.js
@@ -0,0 +1,39 @@
+const players = ["P1 Name", "P2 Name", "P3 Name", "P4 Name", "P5 Name", "P6 Name", "P7 Name", "P8 Name", "P9 Name"];
+
+// Function to populate the table {simplification}
+
+function populateLineup(){
+    const tbody = document.getElementById('lineup-body');
+    players.forEach(player => {
+        const row = document.createElement('tr');
+        row.innerHTML = 
+        <td>${player}</td>;
+        <td>SS</td>;
+        <><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></>;
+        <td class="total-runs">0</td>;
+        tbody.appendChild(row);
+        })
+}
+
+// Fuction to export to CSV for Excel
+
+function exportToExcel(){
+    let csv = 'Player,Pos,1,2,3,4,5,6,7,8,9,R\n';
+}
+
+// Logic to gather data from the table or data object
+
+document.querySelectorAll('#lineup-body tr').forEach(row => {
+    const cells = row.querySelectorAll('td');
+    const rowData = Array.from(cells).map(cell => cell.innerText);
+    csv += rowData.join(',') +'\n';
+})
+
+const blob = new Blob([csv], {type: 'ext/csv;charset=utf-8;'});
+const link = document.createElement("a");
+link.href = YRL.createObjectURL(blob);
+link.setAttribute("download", "baseball_scorecard.csv");
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
+populateLineuo();
