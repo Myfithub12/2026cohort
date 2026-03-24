7bae132ca5b6529f018c805a81f7984929cc663c
Mark Hollrah <mhollrah0001@gmail.com>
2026-03-24 18:43:19 -0400

    deleted server.js page

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-const port = 2025;
-
-app.listen(port, () => {
-    console.log('Server is running os http://localhost:${port}');
-    console.log("Press Ctrl+C to end this process.");
-});
